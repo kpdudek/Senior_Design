606bb9efadb443967ad7fc8faaa9c08717c67ef9
Kurt <kpdudek2@gmail.com>
2019-10-28 12:25:16 -0400

    Fixed joint 4 publisher

--- src/sim_control/src/4joint_mover.py
+++ src/sim_control/src/4joint_mover.py
@@ -8,10 +8,11 @@ from std_msgs.msg import Float64
 def main():
 	rospy.init_node('Joint_Control', anonymous='True')
 
-	J4_pub = rospy.Publisher('/rrbot/joint0_position_controller/command', Float64, queue_size = 1)
 	j1_pub = rospy.Publisher('/rrbot/joint1_position_controller/command', Float64, queue_size = 1)
 	j2_pub = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size = 1)
 	j3_pub = rospy.Publisher('/rrbot/joint3_position_controller/command', Float64, queue_size = 1)
+	J4_pub = rospy.Publisher('/rrbot/joint4_position_controller/command', Float64, queue_size = 1)
+
 
 	rate = rospy.Rate(30)
 
@@ -34,10 +35,11 @@ def main():
 		j2.data = -(angle * .5*amp)
 		j3.data = (angle * 2*amp)
 
-		J4_pub.publish(J4)
 		j1_pub.publish(j1)
 		j2_pub.publish(j2)
 		j3_pub.publish(j3)
+		J4_pub.publish(J4)
+		
 		rate.sleep()
 
 if __name__ == "__main__":
