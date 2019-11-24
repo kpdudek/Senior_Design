9c96ff27efe3e41679fc9ee80b9b879717fff1e9
Kurt <kpdudek2@gmail.com>
2019-11-24 18:20:04 -0500

    Extended joint range

--- src/sim_control/src/IK_Test.py
+++ src/sim_control/src/IK_Test.py
@@ -6,17 +6,24 @@ from math import pi
 import numpy as np
 from std_msgs.msg import Float64
 import threading
-from ikpy.chain import Chain
-import ikpy
+import trac_ik
+from trac_ik_python.trac_ik import IK
+#from trac_ik_python.trac_ik_wrap import TRAC_IK
+
+
 
 def main():
+
     rospy.init_node('Joint_Control', anonymous='True')
 
-    j1_pub = rospy.Publisher('/rrbot/joint1_position_controller/command', Float64, queue_size = 1)
-    j2_pub = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size = 1)
-    j3_pub = rospy.Publisher('/rrbot/joint3_position_controller/command', Float64, queue_size = 1)
-    j4_pub = rospy.Publisher('/rrbot/joint4_position_controller/command', Float64, queue_size = 1)
-
+    j1_pub = rospy.Publisher(
+        '/rrbot/joint1_position_controller/command', Float64, queue_size=1)
+    j2_pub = rospy.Publisher(
+        '/rrbot/joint2_position_controller/command', Float64, queue_size=1)
+    j3_pub = rospy.Publisher(
+        '/rrbot/joint3_position_controller/command', Float64, queue_size=1)
+    j4_pub = rospy.Publisher(
+        '/rrbot/joint4_position_controller/command', Float64, queue_size=1)
 
     rate = rospy.Rate(60)
 
@@ -25,7 +32,9 @@ def main():
     j3 = Float64()
     j4 = Float64()
 
-    robotChain = Chain.from_urdf_file("irac.URDF")
+    with open('irac.urdf', 'r') as file:
+        urdf = file.read()
+    Solver = IK("link1", "link4", urdf_string=urdf)
 
     j1.data = 0
     j2.data = 0
@@ -34,13 +43,13 @@ def main():
     val = pi
 
     while not rospy.is_shutdown():
-
         j1_pub.publish(j1)
         j2_pub.publish(j2)
         j3_pub.publish(j3)
         j4_pub.publish(j4)
-            
+
         rate.sleep()
+
 
 if __name__ == "__main__":
     main()
