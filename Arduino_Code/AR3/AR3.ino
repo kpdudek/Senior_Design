7be1c6e39ecb87a9706b688e098bed155181a683
Kurt <kpdudek2@gmail.com>
2020-01-30 11:53:29 -0500

    Added debuggin custom message

--- Arduino_Code/AR3/AR3.ino
+++ Arduino_Code/AR3/AR3.ino
@@ -1,6 +1,7 @@
 #include <ros.h>
 #include <std_msgs/Float64.h>
 #include <math.h>
+#include <AR3/AR3_Debug.h>
 
 ///////////////////////////////////////////////
 //  Pin definitions and Variables
@@ -15,20 +16,20 @@ int pulDelay = 300;
 float pulseRev = 800, angleTol = .01;
 
 // Joint angle variables
-float joint1Angle = 0, joint1SetAngle = 0, joint1Error;
+float joint1Angle = 0, joint1SetAngle = 0, joint1Error = 0;
 
 ////////////////////////////////
 // ROS Definitions
 ////////////////////////////////
-std_msgs::Float64 joint1PubData;
-ros::Publisher joint1Pub("/AR3/joint1/current_angle", &joint1PubData);
+AR3::AR3_Debug joint1PubData;
+ros::Publisher joint1Pub("/AR3/joint1/debug", &joint1PubData);
 
 void joint1Callback(const std_msgs::Float64& joint1_msg){
     joint1SetAngle = joint1_msg.data;// * 57.2957795;
 
-    joint1PubData.data = joint1Angle;
+    // joint1PubData.data = joint1Angle;
     // joint1PubData.data = joint1Error;
-    joint1Pub.publish(&joint1PubData);
+    // joint1Pub.publish(&joint1PubData);
 }
 
 ros::NodeHandle  nh;
@@ -81,11 +82,13 @@ void loop() {
 
     // Publish the arduinos current angle value for debugging purposes
     // COMMENT OUT FOR SPEED IMPROVEMENT
-    // if ((t-t_old) > (1000/25)){
-    //     joint1PubData.data = joint1Angle;
-    //     // joint1PubData.data = joint1Error;
-    //     joint1Pub.publish(&joint1PubData);
-    // }
+    if ((t-t_old) > (1500)){
+        joint1PubData.j1_current_angle = joint1Angle;
+        joint1PubData.j1_setpoint_angle = joint1SetAngle;
+        joint1PubData.j1_angle_error = joint1Error;
+        joint1Pub.publish(&joint1PubData);
+        t_old = t;
+    }
 
     nh.spinOnce();
 }
@@ -108,38 +111,3 @@ void sendPulse(int pin, int dirPin, int dir) {
   delayMicroseconds(pulDelay);
   digitalWrite(pin,LOW);
 }
-
-// float edgeAngle(float angle1, float angle2){
-//     // function [edgeAngle]=edge_angle(vertex0,vertex1,vertex2,angleType)
-//     // vec1=(vertex1-vertex0)/norm(vertex1-vertex0);%compute the normalized vector from vertex0 to vertex1.
-//     // vec2=(vertex2-vertex0)/norm(vertex2-vertex0);%compute the normalized vector from vertex0 to vertex2.
-
-//     // cosine_theta=vec1'*vec2;%compute the cosine value of the angle using the inner product of two vectors.
-//     // sine_theta=[0 0 1]*cross([vec1;0],[vec2;0]);%compute the sine value of the angle.
-
-//     // edgeAngle=atan2(sine_theta,cosine_theta);
-
-//     // %compute the edge angle
-//     // if strcmpi(angleType,'unsigned')
-//     //     edgeAngle=mod(edgeAngle+2*pi,2*pi);
-//     // elseif strcmpi(angleType,'signed')
-//     //     return;
-//     // else
-//     //     disp('input error');
-//     //     edgeAngle=NaN;
-//     //     return;
-//     // end
-//     float vertex0[2] = {0,0};
-//     float vertex1[2];
-//     float vertex2[2];
-//     vertex1[0] = cos(angle1);
-//     vertex1[1] = sin(angle1);
-//     vertex2[0] = cos(angle2);
-//     vertex2[1] = sin(angle2);
-
-//     float cosine_theta = vertex1[0]*vertex2[0] + vertex1[1]*vertex2[1]; 
-//     float sine_theta = vertex1[0]*vertex2[1]- vertex1[1]*vertex2[0];
-//     float edge_angle = atan2(sine_theta,cosine_theta);
-//     return edge_angle;
-
-// }
