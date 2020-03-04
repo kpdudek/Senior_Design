137f9027d3d645ab0152981e5d2d2208b2b9a986
emcilhenny <emcilhenny@verizon.net>
2020-03-04 12:42:58 -0500

    interpreter node class working

--- src/Teensy/src/InterpreterNodeClass.py
+++ src/Teensy/src/InterpreterNodeClass.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
+from Teensy.msg import AR3_Control
+from AR3.msg import AR3_Feedback
+import rospy
+import time
+from math import sin
+from math import pi
+import numpy as np
+from std_msgs.msg import Float64
+import sys
 
 from Teensy.msg import Teensy_Pulses
-import RobotControllerClass_Ethan import RobotController
+from RobotControllerClass_Ethan import RobotController
 
 
 class InterpreterNode(RobotController):
   def __init__(self):
+    super(InterpreterNode,self).__init__()
     rospy.init_node('Interpreter_Node', anonymous='True')
-    super().__init__()
+    
     
     self.pulsesSub = rospy.Subscriber('/Teensy/encoders', Teensy_Pulses, self.pulseCallback)
 
   def pulseCallback(self,data):
     for i in range(0,5):
-      self.AR3_Control.joint_angles[i] = data.pulses[i] / 2400.0 * 2 * pi
-      if (self.AR3_Control.joint_angles[i] > 2*pi):
-        self.AR3_Control.joint_angles[i] = 0.0 + self.AR3_Control.joint_angles[i] - 2*pi
-      elif (self.AR3_Control.joint_angles[i] < 0.0):
-        self.AR3_Control.joint_angles[i] = self.AR3_Control.joint_angles[i] + 2*pi
+      self.AR3Control.joint_angles[i] = data.pulses[i] / 2400.0 * 2 * pi
+      if (self.AR3Control.joint_angles[i] > 2*pi):
+        self.AR3Control.joint_angles[i] = 0.0 + self.AR3Control.joint_angles[i] - 2*pi
+      elif (self.AR3Control.joint_angles[i] < 0.0):
+        self.AR3Control.joint_angles[i] = self.AR3Control.joint_angles[i] + 2*pi
 
     self.send_joints()
 
--- src/Teensy/src/TestInterClass.py
+++ src/Teensy/src/TestInterClass.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+from Teensy.msg import AR3_Control
+from AR3.msg import AR3_Feedback
+import rospy
+import time
+from math import sin
+from math import pi
+import numpy as np
+from std_msgs.msg import Float64
+import sys
+
 from InterpreterNodeClass import InterpreterNode
 
 def main():
