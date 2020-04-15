ce1d56a8d565196ce7ad1cac2d5e586b170b67a6
Kurt <kpdudek2@gmail.com>
2020-04-15 17:52:14 -0400

    Replay working

--- src/Teensy/src/WaypointReplay.py
+++ src/Teensy/src/WaypointReplay.py
@@ -19,62 +19,57 @@ def check_rob(data):
 
     c = 0
     for ang in list(data.joint_angles):
-        if ang <= 0.95*control.joint_angles[c] or ang >= 1.05*control.joint_angles[c]:
+        if ang <= 0.75*control.joint_angles[c] or ang >= 1.25*control.joint_angles[c]:
             rob_there = False
         c = c + 1
 
 
 def main(argv):
-	global rob_there
-	global control
-    
-	rospy.init_node('Waypoint_Replay', anonymous='True')
+    global rob_there
+    global control
 
-	j_pub = rospy.Publisher(
-	    '/AR3/Control', AR3_Control, queue_size=1)
+    rospy.init_node('Waypoint_Replay', anonymous='True')
+    j_pub = rospy.Publisher('/AR3/Control', AR3_Control, queue_size=1)
+    j_sub = rospy.Subscriber('/AR3/Feedback',AR3_Feedback,check_rob)
+    control = AR3_Control()
+    rate = rospy.Rate(30)
 
-	j_sub = rospy.Subscriber(
-		'/AR3/Feedback',AR3_Feedback,check_rob)
-	
-	control = AR3_Control()
+    try:
+        opts, args = getopt.getopt(argv, "hf:")
+    except getopt.GetoptError:
+        print("No valid argument passed")
+        sys.exit(2)
 
-	rate = rospy.Rate(30)
+    # If no arguments are passed, print help
+    if len(argv) == 0:
+        print("No valid argument passed")
+        sys.exit(2)
 
-	try:
-		opts, args = getopt.getopt(argv, "hf:")
+    # Take in the arguments
+    for opt, arg in opts:
+        if opt in ("-h"):
+            print_info("./WaypointReplay.py -f /path/to/joint/angle/file.txt")
+            sys.exit()
+        elif opt in ("-f"):
+            print("File loaded...")
+            f = open(arg,'r')
 
-	except getopt.GetoptError:
-		print("No valid argument passed")
-		sys.exit(2)
+    waypoint = 1
+    for line in f:
+        #Replay Feature
+        print('Moving to waypoint: %d'%(waypoint))
+        control.joint_angles = list(eval(line))
 
-	# If no arguments are passed, print help
-	if len(argv) == 0:
-		print("No valid argument passed")
-		sys.exit(2)
+        j_pub.publish(control)
+        rob_there = False
 
-	# Take in the arguments
-	for opt, arg in opts:
-		if opt in ("-h"):
-			print_info("./WaypointReplay.py -f /path/to/joint/angle/file.txt")
-			sys.exit()
-		elif opt in ("-f"):
-			print("File loaded...")
-			f = open(arg,'r')
+        while rob_there == False:
+            j_pub.publish(control)
+            rate.sleep()
+            rate.sleep()
+        waypoint += 1
 
-	for line in f:
-
-		#Replay Feature
-		control.joint_angles = list(eval(line))
-		
-		j_pub.publish(control.joint_angles)
-		rob_there = False
-
-		while rob_there == False:
-			rate.sleep()
-		
-		rate.sleep()
-
-	f.close()
+    f.close()
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
